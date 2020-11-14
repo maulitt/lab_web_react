56ac721510894f565de2e96969640b69c5260cfe
regina <brf001@campus.mephi.ru>
2020-11-15 01:17:25 +0300

    still working on appearance (it sucks rn)

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import {useRoutes} from 'hookrouter';
 import {useState} from 'react';
 
+
 function Main() {
   return <h1>Main page</h1>;
 }
@@ -15,61 +16,80 @@ function Register() {
     }
   return (
       <form onSubmit={submit}>
-      <div className="registration">
-        <p><b>Ваше имя</b> <br />
-          <label>
-            <input
-                value={name}
-                name="name"
-                type="text" size="40"
-                onChange={event => { setName(event.target.value) } }
-            />
-          </label>
-        </p>
-        <p><b>Ваш логин (e-mail)</b> <br />
-          <label>
-            <input
-                value={email}
-                name="email"
-                type="text" size="40"
-                onChange={event => { setEmail(event.target.value) } }
-            />
-          </label>
-        </p>
-        <p><b>Пароль</b> <br />
-          <label>
-            <input
-                value={password}
-                name="password"
-                type="text" size="40"
-                onChange={event => { setPassword(event.target.value) } }
-            />
-          </label>
-        </p>
-          <p><input type="submit" value="Submit" /></p>
-      </div>
+          <div id="regi">
+          <div className="registration">
+              <h1>Sign up</h1>
+              <p><b>Ваше имя</b> <br />
+              <label>
+                  <input
+                      value={name}
+                      name="name"
+                      type="text" size="40"
+                      onChange={event => { setName(event.target.value) } }
+                  />
+              </label>
+              </p>
+              <p><b>Ваш логин (e-mail)</b><br />
+              <label>
+                  <input
+                      value={email}
+                      name="email"
+                      type="text" size="40"
+                      onChange={event => { setEmail(event.target.value) } }
+                  />
+              </label>
+              </p>
+              <p><b>Пароль</b> <br />
+              <label>
+                  <input
+                      value={password}
+                      name="password"
+                      type="text" size="40"
+                      onChange={event => { setPassword(event.target.value) } }
+                  />
+              </label>
+              </p>
+              <p><input type="submit" value="Submit" /></p>
+          </div>
+          </div>
       </form>
       );
 }
 
 function Auth() {
-  return (
-      <form>
-          <div className="authentication">
-              <p><b>Ваш логин (e-mail)</b> <br />
-                  <label>
-                      <input name="email" type="text" size="40" />
-                  </label>
-              </p>
-              <p><b>Пароль</b> <br />
-                  <label>
-                      <input name="password" type="text" size="40" />
-                  </label>
-              </p>
-              <p><input type="submit" value="Submit" /></p>
-          </div>
-      </form>
-  )
+    const [email, setEmail] = useState("email");
+    const [password, setPassword] = useState("password");
+    const submit = (e) => {
+        e.preventDefault();
+        alert(password+' '+email);
+    }
+    return (
+        <form onSubmit={submit}>
+            <div className="registration">
+                <p><b>Ваш логин (e-mail)</b> <br />
+                    <label>
+                        <input
+                            value={email}
+                            name="email"
+                            type="text" size="40"
+                            onChange={event => { setEmail(event.target.value) } }
+                        />
+                    </label>
+                </p>
+                <p><b>Пароль</b> <br />
+                    <label>
+                        <input
+                            value={password}
+                            name="password"
+                            type="text" size="40"
+                            onChange={event => { setPassword(event.target.value) } }
+                        />
+                    </label>
+                </p>
+                <p><input type="submit" value="Submit" /></p>
+            </div>
+        </form>
+    );
 }
 
 
@@ -84,9 +104,11 @@ function App() {
   const results = useRoutes(Routes);
   return (
       <div className="App">
-        <a href="/">Main</a>
-        <a href="/register">Registration</a>
-        <a href="/auth">Authentication</a>
+          <ul>
+              <li><a href="/">Main</a></li>
+              <li><a href="/register">Registration</a></li>
+              <li><a href="/auth">Authentication</a></li>
+          </ul>
         {results}
       </div>
   );
