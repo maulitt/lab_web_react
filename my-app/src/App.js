bbea63f6015bc5bff9b3bcf6ab116408a2e07a87
regina <brf001@campus.mephi.ru>
2020-11-14 19:51:17 +0300

    commit of the day - i managed to alert my forms!!!

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,24 +1,94 @@
-import logo from './logo.svg';
 import './App.css';
+import {useRoutes} from 'hookrouter';
+import {useState} from 'react';
+
+function Main() {
+  return <h1>Main page</h1>;
+}
+function Register() {
+    const [name, setName] = useState("Name");
+    const [email, setEmail] = useState("email");
+    const [password, setPassword] = useState("password");
+    const submit = (e) => {
+        e.preventDefault();
+        alert(name+' '+password+' '+email);
+    }
+  return (
+      <form onSubmit={submit}>
+      <div className="registration">
+        <p><b>Ваше имя</b> <br />
+          <label>
+            <input
+                value={name}
+                name="name"
+                type="text" size="40"
+                onChange={event => { setName(event.target.value) } }
+            />
+          </label>
+        </p>
+        <p><b>Ваш логин (e-mail)</b> <br />
+          <label>
+            <input
+                value={email}
+                name="email"
+                type="text" size="40"
+                onChange={event => { setEmail(event.target.value) } }
+            />
+          </label>
+        </p>
+        <p><b>Пароль</b> <br />
+          <label>
+            <input
+                value={password}
+                name="password"
+                type="text" size="40"
+                onChange={event => { setPassword(event.target.value) } }
+            />
+          </label>
+        </p>
+          <p><input type="submit" value="Submit" /></p>
+      </div>
+      </form>
+      );
+}
+
+function Auth() {
+  return (
+      <form>
+          <div className="authentication">
+              <p><b>Ваш логин (e-mail)</b> <br />
+                  <label>
+                      <input name="email" type="text" size="40" />
+                  </label>
+              </p>
+              <p><b>Пароль</b> <br />
+                  <label>
+                      <input name="password" type="text" size="40" />
+                  </label>
+              </p>
+              <p><input type="submit" value="Submit" /></p>
+          </div>
+      </form>
+  )
+}
+
+
+const Routes = {
+  "/": () => <Main />,
+  "/register": () => <Register />,
+  "/auth": () => <Auth />
+}
+
 
 function App() {
+  const results = useRoutes(Routes);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <a href="/">Main</a>
+        <a href="/register">Registration</a>
+        <a href="/auth">Authentication</a>
+        {results}
+      </div>
   );
 }
 
